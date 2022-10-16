f91422e158834e8cb31e4c2bed2722dffbe93725
Sergio Baeza Carrasco <svbc2@alu.ua.es>
2022-10-16 23:59:49 +0200

    Añadido metodo findByIsAdmin en UserRepository

--- src/main/java/madstodolist/model/UsuarioRepository.java
+++ src/main/java/madstodolist/model/UsuarioRepository.java
@@ -2,8 +2,11 @@ package madstodolist.model;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
     Optional<Usuario> findByEmail(String s);
+
+    Optional<Usuario> findByIsAdmin(Boolean isAdmin);
 }
