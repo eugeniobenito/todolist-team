b6d1719f512c142eab2ad60b969be1a9965e895d
Sergio Baeza Carrasco <svbc2@alu.ua.es>
2022-10-16 22:48:09 +0200

    Añadido controlador listarDetallesUsuario

--- src/main/java/madstodolist/controller/UserController.java
+++ src/main/java/madstodolist/controller/UserController.java
@@ -1,12 +1,14 @@
 package madstodolist.controller;
 
 import madstodolist.authentication.ManagerUserSession;
+import madstodolist.controller.exception.UsuarioNotFoundException;
 import madstodolist.model.Usuario;
 import madstodolist.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class UserController {
@@ -28,5 +30,18 @@ public class UserController {
             model.addAttribute("usuario", user);
         }
         return "listaRegistrados";
+    }
+
+    @GetMapping("/registrados/{id}/")
+    public String detallesUsuario(@PathVariable(value="id") Long idUsuario, Model model){
+        Usuario usuario = usuarioService.findById(idUsuario);
+
+        if(usuario == null){
+            throw new UsuarioNotFoundException();
+        }
+
+        model.addAttribute("usuario", usuario); 
+
+        return "detallesUsuario";
     }
 }
