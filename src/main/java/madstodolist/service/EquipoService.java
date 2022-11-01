63601a751a499636fe37fe9db1bcfbd5ead3a286
Sergio Baeza Carrasco <svbc2@alu.ua.es>
2022-11-01 18:47:21 +0100

    añadir usuario a equipo

--- src/main/java/madstodolist/service/EquipoService.java
+++ src/main/java/madstodolist/service/EquipoService.java
@@ -2,12 +2,15 @@ package madstodolist.service;
 
 import madstodolist.model.Equipo;
 import madstodolist.model.EquipoRepository;
+import madstodolist.model.Usuario;
+import madstodolist.model.UsuarioRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,7 +18,11 @@ public class EquipoService {
     Logger logger = LoggerFactory.getLogger(EquipoService.class);
 
     @Autowired
-    EquipoRepository equipoRepository;
+    private EquipoRepository equipoRepository;
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
 
     @Transactional
     public Equipo crearEquipo(String nombre) {
@@ -30,7 +37,21 @@ public class EquipoService {
     }
 
     @Transactional(readOnly = true)
-    public List<Equipo> findAllOrderedByName(){
+    public List<Equipo> findAllOrderedByName() {
         return equipoRepository.findAllByOrderByNombreAsc();
+    }
+
+    @Transactional
+    public void addUsuarioEquipo(Long userId, Long equipoId) {
+        Usuario user = usuarioRepository.findById(userId).orElse(null);
+        Equipo equipo = equipoRepository.findById(equipoId).orElse(null);
+        equipo.addUsuario(user);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Usuario> usuariosEquipo(Long equipoId) {
+        Equipo e = equipoRepository.findById(equipoId).orElse(null);
+        List<Usuario> l = new ArrayList<>(e.getUsuarios());
+        return l; 
     }
 }
