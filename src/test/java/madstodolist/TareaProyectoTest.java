331f34c346d2e87104335a4658c60f13d5bbf2c9
Sergio Baeza <svbc2@alu.ua.es>
2022-12-18 00:13:51 +0100

    TareaProyectoRepository creado

--- src/test/java/madstodolist/TareaProyectoTest.java
+++ src/test/java/madstodolist/TareaProyectoTest.java
@@ -2,6 +2,7 @@ package madstodolist;
 
 import madstodolist.model.*;
 import org.assertj.core.api.Assertions;
+import org.checkerframework.checker.units.qual.A;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,15 @@ import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TES
 @SpringBootTest
 @Sql(scripts = "/clean-db.sql", executionPhase = AFTER_TEST_METHOD)
 public class TareaProyectoTest {
+
+    @Autowired
+    EquipoRepository equipoRepository;
+
+    @Autowired
+    ProyectoRepository proyectoRepository;
+
+    @Autowired
+    TareaProyectoRepository tareaProyectoRepository;
 
     @Test
     public void crearTareaProyectoConRelaciones() {
@@ -24,6 +34,17 @@ public class TareaProyectoTest {
         Assertions.assertThat(tp.getStatus()).isEqualTo(Status.TODO);
         tp.setStatus(Status.DONE);
         Assertions.assertThat(tp.getStatus()).isEqualTo(Status.DONE);
+    }
+
+    @Test
+    public void tareaProyectoCanSave() {
+        Equipo e = new Equipo("aaa");
+        equipoRepository.save(e);
+        Proyecto p = new Proyecto("prueba", e);
+        proyectoRepository.save(p);
+        TareaProyecto tp = new TareaProyecto("estudiar", p);
+        tp = tareaProyectoRepository.save(tp);
+        Assertions.assertThat(tp.getId()).isNotNull(); 
     }
 
 
