920d36a6f32fd29259d9065aa61b4952dbad5cf9
Sergio Baeza <svbc2@alu.ua.es>
2022-12-17 23:39:26 +0100

    Controladores get y post anyadir proyecto

---
+++ src/main/java/madstodolist/controller/ProyectoController.java
@@ -0,0 +1,92 @@
+package madstodolist.controller;
+
+import madstodolist.authentication.ManagerUserSession;
+import madstodolist.controller.exception.*;
+import madstodolist.model.ComentarioEquipo;
+import madstodolist.model.Equipo;
+import madstodolist.model.Proyecto;
+import madstodolist.model.Usuario;
+import madstodolist.service.ComentarioEquipoService;
+import madstodolist.service.EquipoService;
+import madstodolist.service.ProyectoService;
+import madstodolist.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class ProyectoController {
+
+    @Autowired
+    EquipoService equipoService;
+
+    @Autowired
+    ManagerUserSession managerUserSession;
+
+    @Autowired
+    ProyectoService proyectoService;
+
+    @Autowired
+    UsuarioService usuarioService;
+
+    private void checkAdminOfTeam(Equipo e) {
+        if (!managerUserSession.isUsuarioLogeado())
+            throw new UsuarioNoLogeadoException();
+        Long idUser = managerUserSession.usuarioLogeado();
+        Usuario u = usuarioService.findById(idUser);
+        Long idAdmin = new Long(-1);
+        if(e.getAdmin() != null)  idAdmin = e.getAdmin().getId();
+        if(!u.getIsAdmin() && idUser != idAdmin){
+            throw new UsuarioNoAdminException();
+        }
+    }
+
+    @Transactional
+    private void userJoinedTeam(Equipo e) {
+        if (!managerUserSession.isUsuarioLogeado())
+            throw new UsuarioNoLogeadoException();
+        Long idUser = managerUserSession.usuarioLogeado();
+        Usuario u = usuarioService.findById(idUser);
+        Long idAdmin = new Long(-1);
+        if(e.getAdmin() != null)  idAdmin = e.getAdmin().getId();
+        if(!equipoService.usuarioPerteneceEquipo(e, u) && idUser != idAdmin){
+            throw new UsuarioNotJoinedTeamException();
+        }
+    }
+
+
+    @GetMapping("/equipos/{id}/proyectos/nuevo")
+    public String formNuevoProyecto(@PathVariable(value="id") Long idEquipo,
+                                 @ModelAttribute ProyectoData proyectoData, Model model,
+                                 HttpSession session) {
+        Equipo equipo = equipoService.recuperarEquipo(idEquipo);
+        if(equipo == null)
+            throw new EquipoNotFoundException();
+        checkAdminOfTeam(equipo);
+        model.addAttribute("equipo",equipo);
+        return "formNuevoProyecto";
+    }
+
+    @PostMapping("/equipos/{id}/proyectos")
+    public String nuevoProyecto(@PathVariable(value="id") Long idEquipo,
+                                  @ModelAttribute ProyectoData proyectoData,
+                                  Model model, RedirectAttributes flash,
+                                  HttpSession session) {
+
+        Equipo equipo = equipoService.recuperarEquipo(idEquipo);
+        if(equipo == null)
+            throw new EquipoNotFoundException();
+        checkAdminOfTeam(equipo);
+
+        if(proyectoData.getNombre() == "") throw new FormErrorException();
+        Proyecto proyecto = proyectoService.crearProyecto(proyectoData.getNombre(), equipo.getId());
+
+        return "redirect:/equipos/" + idEquipo;
+    }
+
+}
