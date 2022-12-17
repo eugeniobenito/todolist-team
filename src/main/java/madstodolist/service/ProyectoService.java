ccf70a6fbb272f2625ff1590122162c714e6e78c
Sergio Baeza <svbc2@alu.ua.es>
2022-12-17 23:04:20 +0100

    ProyectoServicio metodo nuevoProyecto

---
+++ src/main/java/madstodolist/service/ProyectoService.java
@@ -0,0 +1,29 @@
+package madstodolist.service;
+
+import madstodolist.model.Equipo;
+import madstodolist.model.EquipoRepository;
+import madstodolist.model.Proyecto;
+import madstodolist.model.ProyectoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class ProyectoService {
+    @Autowired
+    ProyectoRepository proyectoRepository;
+
+    @Autowired
+    EquipoRepository equipoRepository;
+
+    @Transactional
+    public Proyecto crearProyecto(String nombre, Long equipoId){
+        Equipo e = equipoRepository.findById(equipoId).orElse(null);
+        if(e == null) {
+            // TO DO
+        }
+        Proyecto p = new Proyecto(nombre, e);
+        p = proyectoRepository.save(p);
+        return p;
+    }
+}
--- src/test/java/madstodolist/ProyectoServiceTest.java
+++ src/test/java/madstodolist/ProyectoServiceTest.java
@@ -1,46 +1,38 @@
 package madstodolist;
 
 import madstodolist.model.Equipo;
+import madstodolist.model.EquipoRepository;
 import madstodolist.model.Proyecto;
 import madstodolist.model.ProyectoRepository;
-import madstodolist.model.EquipoRepository;
+import madstodolist.service.ProyectoService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.jdbc.Sql;
-
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 
 @SpringBootTest
 @Sql(scripts = "/clean-db.sql", executionPhase = AFTER_TEST_METHOD)
-public class ProyectoTest {
+public class ProyectoServiceTest {
     @Autowired
     ProyectoRepository proyectoRepository;
 
     @Autowired
     EquipoRepository equipoRepository;
 
+    @Autowired
+    ProyectoService proyectoService;
 
     @Test
     public void entidadProyecto(){
         Equipo e = new Equipo("aaa");
-        Proyecto p = new Proyecto("prueba", e);
-        Assertions.assertThat(p.getNombre()).isEqualTo("prueba");
+        e = equipoRepository.save(e);
+        Proyecto p = proyectoService.crearProyecto("prueba", e.getId());
         Assertions.assertThat(p.getEquipo()).isEqualTo(e);
+        e = equipoRepository.findById(e.getId()).orElse(null); 
         Assertions.assertThat(e.getProyectos().size()).isEqualTo(1);
     }
-
-    @Test
-    public void proyectoCanSave(){
-        Equipo e = new Equipo("aaa");
-        equipoRepository.save(e);
-        Proyecto p = new Proyecto("prueba", e);
-        proyectoRepository.save(p);
-        Assertions.assertThat(p.getId()).isNotNull();
-    }
-
 }
