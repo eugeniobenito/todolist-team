a7c634bdfe3135a0c4c13dfc4267d6cf223ca0de
Sergio Baeza Carrasco <svbc2@alu.ua.es>
2022-12-20 22:48:12 +0100

    Cambiar version de 1.4.0-SNAPSHOT a 1.4.0

--- src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-/* Populate tables */
-INSERT INTO usuarios (id, code, email, nombre, password, fecha_nacimiento, is_admin, blocked) VALUES('1', '32123', 'user@ua', 'Usuario Ejemplo', '123', '2001-02-10', false, false);
-INSERT INTO tareas (id, titulo, usuario_id) VALUES('1', 'Lavar coche', '1');
-INSERT INTO tareas (id, titulo, usuario_id) VALUES('2', 'Renovar DNI', '1');
