63d5ec69f4caea955fdb1ae5d7a9a0653f56c03f
Eugenio Benito <eugeniobenito00@gmail.com>
2022-12-20 15:35:07 +0100

    Añade controller aceptar invitacion

--- src/main/java/madstodolist/controller/InvitacionController.java
+++ src/main/java/madstodolist/controller/InvitacionController.java
@@ -3,6 +3,7 @@ package madstodolist.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -81,6 +82,15 @@ public class InvitacionController {
         Invitacion invitacion = invitacionService.findById(idInvitacion);
         comprobarUsuarioLogeado(invitacion.getUsuarioId());
         invitacionService.denegar(invitacion);
+        return "";
+    }
+
+    @PatchMapping("/invitacion/{id}")
+    @ResponseBody
+    public String aceptar(@PathVariable(value="id") Long idInvitacion) {
+        Invitacion invitacion = invitacionService.findById(idInvitacion);
+        comprobarUsuarioLogeado(invitacion.getUsuarioId());
+        invitacionService.aceptar(invitacion);
         return "";
     }
 }
--- src/main/resources/static/js/invitations.js
+++ src/main/resources/static/js/invitations.js
@@ -1,9 +1,19 @@
-function deleteInvitation(urlBorrar) {
+function aceptInvitacion(urlPatch) {
+    console.log("hey");
+    const url = 'http://localhost:8080' + urlPatch;
+    console.log(url);
+    fetch(url, {
+        method: 'PATCH'
+    }).then((res) => {
+        location.reload();
+    })
+}
+
+function deleteInvitacion(urlBorrar) {
     const url = 'http://localhost:8080' + urlBorrar;
     fetch(url, {
         method: 'DELETE'
     }).then((res) => {
         location.reload();
     })
-
 }
