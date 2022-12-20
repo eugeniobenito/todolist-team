84d6487a3ca1d4e11a02e9e10134116739c7b63e
Sergio Baeza Carrasco <svbc2@alu.ua.es>
2022-12-20 23:40:36 +0100

    data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+/* Populate tables */
+INSERT INTO usuarios (id, code, email, nombre, password, fecha_nacimiento, is_admin, blocked) VALUES('1', '32123', 'user@ua', 'Usuario Ejemplo', '123', '2001-02-10', false, false);
+
